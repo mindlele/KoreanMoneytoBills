3a465864586ee85067f7b2956f7e2623a04db7e1
mindlele <minhyejung0404@gmail.com>
2022-02-28 14:55:40 +0000

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let color = "black"
+//Array of Korean bills and coins
+let billCoinArray = [50000,10000,5000,1000,500,100]
 
-console.log(color)
+function moneyChangeToBill(amount){
+ if (amount>0){
+  for(i=0; i<billCoinArray.length; i++) {
+   if(amount>=billCoinArray[i]){
+     console.log(billCoinArray[i] + "원 짜리는 " + Math.floor(amount/billCoinArray[i]) + "개 필요")
+     amount = amount - billCoinArray[i]*Math.floor(amount/billCoinArray[i])
+   }else {
+    continue;
+   }
+  }
+ }else {
+ console.log("값이 0 또는 음수입니다.")
+ }
+}
+
+moneyChangeToBill(52400)
 
